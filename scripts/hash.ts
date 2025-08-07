ffa36221fab19172cf5d7c3d2fc1523f1059c481
Rene Miranda <renemirandau4@gmail.com>
2025-08-06 22:26:09 -0600

    ->feat-> adding hashing script, adding bcrypt dev dependency

---
+++ scripts/hash.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env tsx
+import * as bcrypt from 'bcrypt';
+
+const password = process.argv[2];
+
+if (!password) {
+  console.error('❌ Please provide a password to hash.');
+  process.exit(1);
+}
+
+bcrypt.hash(password, 12).then((hash) => {
+  console.log('\n✅ Hashed password:');
+  console.log(hash);
+});
+
+// npx tsx scripts/hash.ts yourPassword
